baba4fdacbab327ff20d699a90168c40d3fe2434
Divyanshi Dwivedi - Vendor <ddwive1@l-sb8r1t8fd5-m.local>
2016-05-09 14:10:40 -0700

    Initial Commit

---
+++ tip_calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tip_calculator
+//
+//  Created by Divyanshi Dwivedi - Vendor on 5/9/16.
+//  Copyright © 2016 Divyanshi Dwivedi - Vendor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
